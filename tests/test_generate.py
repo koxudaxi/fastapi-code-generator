33278d83afd20d1417845d43a9a5c0e74c41d6ca
Roberto Polli <robipolli@gmail.com>
2020-06-18 21:21:36 +0200

    Remove unused variable.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -5,6 +5,5 @@ import pytest
 
 @pytest.mark.parametrize("oas_file", Path("tests/").glob("*.yaml"))
 def test_generate_simple(oas_file):
-    input_file = oas_file  # "tests" / Path('simple.yaml')
-    ret = generate_code(input_name=input_file.name, input_text=input_file.read_text(), output_dir="tmp" / oas_file.with_suffix(".app"),
+    ret = generate_code(input_name=oas_file.name, input_text=oas_file.read_text(), output_dir="tmp" / oas_file.with_suffix(".app"),
                         template_dir=None)
