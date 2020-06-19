d3f8d97733bd5369da81014fb03f9cf868c18978
Koudai Aono <koxudaxi@gmail.com>
2020-06-19 12:59:03 +0900

    fix pytest-cov setting

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-poetry run pytest --cov=fastapi_code_generator --cov-report term-missing tests
+poetry run pytest --cov=fastapi_code_generator --cov-report=xml --cov-report term-missing tests
