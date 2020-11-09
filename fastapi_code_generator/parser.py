8c02637d4b91723bd708877477ca39a27f249a5e
Koudai Aono <koxudaxi@gmail.com>
2020-11-09 15:42:31 +0900

    fix typehint

--- fastapi_code_generator/parser.py
+++ fastapi_code_generator/parser.py
@@ -21,7 +21,7 @@ MODEL_PATH = ".models"
 
 model_path_var: ContextVar[str] = ContextVar('model_path', default=MODEL_PATH)
 
-APPLICATION_JSON_PATTERN: Pattern = re.compile(r'^application/.*json$')
+APPLICATION_JSON_PATTERN: Pattern[str] = re.compile(r'^application/.*json$')
 
 
 class CachedPropertyModel(BaseModel):
